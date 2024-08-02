b34b7121ea91d8f17e95f635ac3cf55da5965da2
HabibaMohamed111 <167786822+HabibaMohamed111@users.noreply.github.com>
2024-08-02 22:14:05 +0300

    Update Import CSV1.py

--- Import CSV1.py
+++ Import CSV1.py
@@ -11,9 +11,9 @@ df1.head()
 
 # OutPut
 
-	property_type	 place_with_parent_names	 region	         lat-lon	        area_m2	   price_usd
-0 	apartment	  |Brasil|Alagoas|Maceió|	  Northeast	 -9.6443051,-35.7088142 	110.0	  $187,230.85
+	property_type	 place_with_parent_names	 region	         lat-lon	          area_m2	   price_usd
+0 	apartment	  |Brasil|Alagoas|Maceió|	  Northeast	 -9.6443051,-35.7088142     110.0	  $187,230.85
 1	 apartment	  |Brasil|Alagoas|Maceió|	  Northeast	 -9.6430934,-35.70484	    65.0	  $81,133.37
-2	 house	      |Brasil|Alagoas|Maceió|	  Northeast	 -9.6227033,-35.7297953 	211.0	  $154,465.45
-3 	apartment	  |Brasil|Alagoas|Maceió|	  Northeast	 -9.622837,-35.719556	   99.0	    $146,013.20
-4	 apartment	  |Brasil|Alagoas|Maceió| 	Northeast	 -9.654955,-35.700227	   55.0	    $101,416.71
+2	 house	          |Brasil|Alagoas|Maceió|	  Northeast	 -9.6227033,-35.7297953     211.0	  $154,465.45
+3 	apartment	  |Brasil|Alagoas|Maceió|	  Northeast	 -9.622837,-35.719556	     99.0	  $146,013.20
+4	 apartment	  |Brasil|Alagoas|Maceió| 	  Northeast	 -9.654955,-35.700227	     55.0	  $101,416.71
